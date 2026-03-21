d664aa86991e19a532a51cff8935d9ba37d5c233
andreklaroskleao <andre.klar.leao@gmail.com>
2026-03-21 17:45:55 -0300

    Update firebade-init.js

--- assets/js/config/firebade-init.js
+++ assets/js/config/firebade-init.js
@@ -5,12 +5,12 @@ import {
 } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: 'SUA_API_KEY',
-  authDomain: 'SEU_AUTH_DOMAIN',
-  projectId: 'SEU_PROJECT_ID',
-  storageBucket: 'SEU_STORAGE_BUCKET',
-  messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
-  appId: 'SEU_APP_ID'
+  apiKey: "AIzaSyA9DT2n8NbC_Od4e6HLyDvS_c2WNMgLFW8",
+  authDomain: "horalivre-7c86c.firebaseapp.com",
+  projectId: "horalivre-7c86c",
+  storageBucket: "horalivre-7c86c.firebasestorage.app",
+  messagingSenderId: "334858873661",
+  appId: "1:334858873661:web:b28d3617a887f84fe8f354"
 };
 
 const app = initializeApp(firebaseConfig);
