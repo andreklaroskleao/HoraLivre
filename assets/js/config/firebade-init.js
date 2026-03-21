65ccaf8e035f9a342927588256150b1d1b8a5f71
andreklaroskleao <andre.klar.leao@gmail.com>
2026-03-21 18:03:10 -0300

    Atualizar o firebade-init.js

--- assets/js/config/firebade-init.js
+++ assets/js/config/firebade-init.js
@@ -1,16 +1,14 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
-import {
-  getFirestore
-} from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9DT2n8NbC_Od4e6HLyDvS_c2WNMgLFW8",
-  authDomain: "horalivre-7c86c.firebaseapp.com",
-  projectId: "horalivre-7c86c",
-  storageBucket: "horalivre-7c86c.firebasestorage.app",
-  messagingSenderId: "334858873661",
-  appId: "1:334858873661:web:b28d3617a887f84fe8f354"
+  apiKey: 'AIzaSyA9DT2n8NbC_Od4e6HLyDvS_c2WNMgLFW8',
+  authDomain: 'horalivre-7c86c.firebaseapp.com',
+  projectId: 'horalivre-7c86c',
+  storageBucket: 'horalivre-7c86c.firebasestorage.app',
+  messagingSenderId: '334858873661',
+  appId: '1:334858873661:web:b28d3617a887f84fe8f354'
 };
 
 const app = initializeApp(firebaseConfig);
